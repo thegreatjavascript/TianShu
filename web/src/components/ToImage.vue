2441294b8a4c35154d49fb69f9da752ac17947b5
林水溶 <shuirong1997@icloud.com>
2021-04-29 00:29:30 +0800

    增加竖排绘制字体方法，支持自动换行；

--- web/src/components/ToImage.vue
+++ web/src/components/ToImage.vue
@@ -59,7 +59,12 @@
     <div class="sketchpad">
       <textarea class="content" v-model="content"></textarea>
       <div class="result">
-        <canvas id="canvas" style="display:block" width="0" height="0"></canvas>
+        <canvas
+          id="canvas"
+          style="display:block"
+          width="300"
+          height="500"
+        ></canvas>
         <img id="image" :src="image" />
       </div>
     </div>
@@ -76,15 +81,14 @@ export default {
   data() {
     return {
       content:
-        "暮投石壕村，有吏夜捉人。老翁逾墙走，老妇出门看。吏呼一何怒！妇啼一何苦。听妇前致词，三男邺城戍。一男附书至，二男新战死。存者且偷生，死者长已矣！室中更无人，惟有乳下孙。有孙母未去，出入无完裙。老妪力虽衰，请从吏夜归。急应河阳役，犹得备晨炊。夜久语声绝，如闻泣幽咽。天明登前途，独与老翁别。",
+        "时势造英雄。当年web网页都是以图文展示为主，所以门户，博客这些网站兴起。而SVG擅长的是图形展示，对于文本呈现，只能嘿嘿一下，跟CSS相比那可就弱了几条街。举个最简单的效果，文本换行。在CSS下，当我们一段文字很长的时候，是会自动换到下一行的，但是在SVG中，基本上就是要手动控制换行，对于新闻、博客展示而言简直就是噩梦。",
       style: {
-        letterSpacing: "-2px",
+        letterSpacing: "0px",
         fontSize: "20px",
         fontWeight: "400",
         textHeight: 1, // 文字高度
         textWidth: 1, // 文字宽度
-        skewX: "50deg" // 倾斜角度，正是向左。
-        // transform: `skewX(` + skewX + `deg) scaleY(` + spanScaleY + `)`,
+        skewX: "0deg" // 倾斜角度，正是向左。
       },
       image: ""
     };
@@ -125,22 +129,21 @@ export default {
     },
     drawText() {
       // 把字符画到 canvas 中
-      const text = document.querySelector(".content");
+      // const text = document.querySelector(".content");
       const canvas = document.querySelector("#canvas");
       const ctx = canvas.getContext("2d");
 
-      canvas.width = text.offsetWidth;
-      canvas.height = text.offsetHeight;
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       canvas.style.letterSpacing = this.style.letterSpacing;
       ctx.scale(this.style.textWidth, this.style.textHeight);
-      ctx.rotate((10 * Math.PI) / 180);
+      ctx.rotate((0 * Math.PI) / 180);
       ctx.font = `${this.style.fontWeight} ${this.style.fontSize} serif`;
       ctx.textBaseline = "top";
+      ctx.textAlign = "left";
 
       ctx.beginPath();
-      ctx.wrapText(this.content, 0, 0, canvas.width, 20);
+      ctx.wrapText(this.content, 0, 0);
     }
   },
   components: {
--- web/src/utils/wrapText.js
+++ web/src/utils/wrapText.js
@@ -1,3 +1,4 @@
+// 支持自动换行的文字绘制
 CanvasRenderingContext2D.prototype.wrapText = function(
   text,
   x,
@@ -5,38 +6,106 @@ CanvasRenderingContext2D.prototype.wrapText = function(
   maxWidth,
   lineHeight
 ) {
-  if (typeof text != 'string' || typeof x != 'number' || typeof y != 'number') {
-    return
+  if (typeof text != "string" || typeof x != "number" || typeof y != "number") {
+    return;
   }
 
-  var context = this
-  var canvas = context.canvas
+  var context = this;
+  var canvas = context.canvas;
 
-  if (typeof maxWidth == 'undefined') {
-    maxWidth = (canvas && canvas.width) || 300
+  if (typeof maxWidth == "undefined") {
+    maxWidth = (canvas && canvas.width) || 300;
   }
-  if (typeof lineHeight == 'undefined') {
+  if (typeof lineHeight == "undefined") {
     lineHeight =
       (canvas && parseInt(window.getComputedStyle(canvas).lineHeight)) ||
-      parseInt(window.getComputedStyle(document.body).lineHeight)
+      parseInt(window.getComputedStyle(document.body).lineHeight) ||
+      20;
   }
 
   // 字符分隔为数组
-  var arrText = text.split('')
-  var line = ''
+  var arrText = text.split("");
+  var line = "";
 
   for (var n = 0; n < arrText.length; n++) {
-    var testLine = line + arrText[n]
-    var metrics = context.measureText(testLine)
-    var testWidth = metrics.width
+    var testLine = line + arrText[n];
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
     if (testWidth > maxWidth && n > 0) {
-      // console.log('text', line, '| line-height:', lineHeight)
-      context.fillText(line, x, y)
-      line = arrText[n]
-      y += lineHeight
+      context.fillText(line, x, y);
+      line = arrText[n];
+      y += lineHeight;
     } else {
-      line = testLine
+      line = testLine;
     }
   }
-  context.fillText(line, x, y)
-}
+  context.fillText(line, x, y);
+  // canvas.height = y;
+};
+
+/**
+ * 文字竖排绘制，支持中英文，支持自动换行，默认从左到右。
+ */
+CanvasRenderingContext2D.prototype.fillTextVertical = function(text, x, y) {
+  var context = this;
+
+  var arrText = text.split("");
+  var arrWidth = arrText.map(function(letter) {
+    return context.measureText(letter).width;
+  });
+  const max = Math.max(...arrWidth);
+
+  var align = context.textAlign;
+  var baseline = context.textBaseline;
+
+  if (align == "left") {
+    x = x + Math.max.apply(null, arrWidth) / 2;
+  } else if (align == "right") {
+    x = x - Math.max.apply(null, arrWidth) / 2;
+  }
+  if (
+    baseline == "bottom" ||
+    baseline == "alphabetic" ||
+    baseline == "ideographic"
+  ) {
+    y = y - arrWidth[0] / 2;
+  } else if (baseline == "top" || baseline == "hanging") {
+    y = y + arrWidth[0] / 2;
+  }
+
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+
+  let maxHeight = (context.canvas && context.canvas.height) || 300;
+  // 开始逐字绘制
+  arrText.forEach(function(letter, index) {
+    // 如果y超出当前高度，或者下一次绘制就会超出，就直接绘制下一列算了。
+    if (y > maxHeight || y + max / 2 > maxHeight) {
+      // 找到字符中，最大的那一个的宽度。
+      x += max;
+      y = max / 2;
+    }
+    // 确定下一个字符的纵坐标位置
+    var letterWidth = arrWidth[index];
+    // 是否需要旋转判断
+    var code = letter.charCodeAt(0);
+    if (code <= 256) {
+      context.translate(x, y);
+      // 英文字符，旋转90°
+      context.rotate((90 * Math.PI) / 180);
+      context.translate(-x, -y);
+    } else if (index > 0 && text.charCodeAt(index - 1) < 256) {
+      // y修正
+      y = y + arrWidth[index - 1] / 2;
+    }
+    context.fillText(letter, x, y);
+    // 旋转坐标系还原成初始态
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    // 确定下一个字符的纵坐标位置
+    var letterWidth = arrWidth[index];
+    y = y + letterWidth;
+  });
+  // 水平垂直对齐方式还原
+  context.textAlign = align;
+  context.textBaseline = baseline;
+};
